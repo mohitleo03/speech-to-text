74b59ded3913bc42d1585e9e79e96fc1f0a9105b
mohitleo03 <sushilkumarmalikfootwear@gmail.com>
2023-08-04 19:24:09 +0530

    using local storage for showing name

--- index.js
+++ index.js
@@ -3,14 +3,48 @@ const fileUpload = require("express-fileupload");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+const bodyParser = require('body-parser');
+const { PythonShell } = require('python-shell');
+app.use(bodyParser.raw({ type: 'audio/wav', limit: '100mb' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(express.static("public"));
-app.use("/getAudio", (request, response, next) => {
+app.use("/getAudio", async(request, response, next) => {
   if (request.files && Object.keys(request.files).length !== 0) {
     const uploadedFile = request.files.audio;
     console.log(`file size is = ${uploadedFile.size} & file name is ${uploadedFile.name}`);
+    const options = {
+        mode: 'text',
+        pythonOptions: ['-m'],
+        pythonPath: 'python3', // Replace with the path to your Python executable
+        scriptPath: './', // Use the current directory as the script path
+        args: ['-'], // Pass '-' as an argument to read audio from stdin
+      };
+  
+      // Execute the Python script with vosk
+      const pyshell = new PythonShell('speech_to_text.py', options);
+  
+      // Handle the Python script output
+      let transcription = '';
+      pyshell.stdout.on('data', function (data) {
+        transcription += data;
+      });
+  
+      // Handle errors
+      pyshell.on('error', function (err) {
+        console.error('Error during speech-to-text conversion:', err);
+        response.status(500).json({ error: 'An error occurred during speech-to-text conversion.' });
+      });
+  
+      // When the Python script finishes, send the transcription back to the frontend
+      pyshell.on('close', function () {
+        console.log('Transcription:', transcription);
+        response.status(200).json({ transcription });
+      });
+  
+      // Send the audio data to the Python script through stdin
+      pyshell.send(uploadedFile);
     response
       .status(200)
       .json({
